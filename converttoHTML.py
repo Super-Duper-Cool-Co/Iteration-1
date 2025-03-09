4fefd4a98666cbb7a92bfb01ece7afac878451a8
Jonathan Forrester <jonnyq8@gmail.com>
2025-03-09 15:18:43 -0400

    updated html

---
+++ converttoHTML.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+columns = ["Animal","Class","Diet","Province","Endangered Status","Population","Native/Invasive","Habitat"]
+df = pd.read_csv('SDCC_Database.csv', names=columns)
+
+# # This you can change it to whatever you want to get
+# age_15 = df[df['age'] == 'U15']
+# # Other examples:
+# bye = df[df['opp'] == 'Bye']
+# crushed_team = df[df['ACscr'] == '0']
+# crushed_visitor = df[df['OPPscr'] == '0']
+# # Play with this
+
+# Use the .to_html() to get your table in html
+print(df.to_html())
