104744a6818093b6149347feb6038c66d7eac968
Jonathan Forrester <jonnyq8@gmail.com>
2025-03-18 19:55:51 -0400

    updated backend

--- Animal.py
+++ Animal.py
@@ -1,10 +1,13 @@
 class Animal:
     def __init__(self, name,type, diet, province, endangerStatus, population, invasiveStatus, habitat ):
-        self.name=name
-        self.type=type
-        self.diet=diet
-        self.province=province
-        self.endagerStatus=endangerStatus
+        self.name=name.strip()
+        self.type=type.strip()
+        self.diet=diet.strip()
+        self.province=province.strip()
+        self.endagerStatus=endangerStatus.strip()
         self.population=population
-        self.invasiveStatus=invasiveStatus
-        self.habitat=habitat
+        self.invasiveStatus=invasiveStatus.strip()
+        self.habitat=habitat.strip()
+
+    def toString(self):
+        print( f"Name: {self.name}, Type: {self.type}, Diet: {self.diet}, Province: {self.province}, Endanger Status: {self.endagerStatus}, Population: {self.population}, Invasive Status: {self.invasiveStatus}, Habitat: {self.habitat}")
--- bakendtest.py
+++ bakendtest.py
@@ -7,29 +7,39 @@ from Animal import *
 import unittest
 
 class TestStringMethods(unittest.TestCase):
-    ZebraMussel = Animal("Zebra Mussel", "Mollusk", "Herbivore", "QC", "LC", 7.5E+14, "Invasive", "Freshwater lakes;Rivers")
-    Moose = Animal("Moose", "Mammal", "Herbivore", "BC", "LC", 20000, "Native", "Woodlands")
-    animalList = {ZebraMussel, Moose}
+    ZebraMussel = Animal("Zebra Mussel", "Mollusk", "Herbivore", "QC", "LC", "7.5E+14", "Invasive", "Freshwater lakes;Rivers")
+    Moose = Animal("Moose", "Mammal", "Herbivore", "BC", "LC", "20000", "Native", "Woodlands")
+    fox = Animal("fox", "Mammal", "Carnivore", "BC", "LC", "20000", "Native", "Woodlands")
+    animalList = {ZebraMussel, Moose, fox}
 
     def test_findAnimalByProvince(self):
-        ZebraMussel = Animal("Zebra Mussel", "Mollusk", "Herbivore", "QC", "LC", 7.5E+14, "Invasive", "Freshwater lakes;Rivers")
-        Moose = Animal("Moose", "Mammal", "Herbivore", "BC", "LC", 20000, "Native", "Woodlands")
-        animalList = {ZebraMussel, Moose}
-        result = find_by_province(animalList, "BC")  # Corrected to pass the province
-        self.assertEqual(result, [Moose]) # Corrected to check for a list containing moose.
+        result = find_by_province(self.animalList, "BC")
+        self.assertEqual(result, [self.Moose,self.fox])
 
-        result_qc= find_by_province(animalList, "QC")
-        self.assertEqual(result_qc, [ZebraMussel])
+        result_qc = find_by_province(self.animalList, "QC")
+        self.assertEqual(result_qc, [self.ZebraMussel])
 
-        result_none = find_by_province(animalList, "AB")
-        self.assertEqual(result_none, [])
+        result_none = find_by_province(self.animalList, "AB")
+        self.assertEqual(result_none, None)
 
     def test_findAnimalByDiet(self):
         result = search_by_diet(self.animalList, "Herbivore")
-        self.assertEqual(result, [self.ZebraMussel, self.Moose])
+        self.assertEqual(result, [self.ZebraMussel,self.Moose])
 
         result_carnivore = search_by_diet(self.animalList, "Carnivore")
-        self.assertEqual(result, [])
+        self.assertEqual(result_carnivore, [self.fox])
 
+    def test_getAnimalDescritpion(self):
+        ZebraMussel = Animal("Zebra Mussel", "Mollusk", "Herbivore", "QC", "LC", "7.5E+14", "Invasive", "Freshwater lakes;Rivers")
+        Moose = Animal("Moose", "Mammal", "Herbivore", "BC", "LC", "20000", "Native", "Woodlands")
+        fox = Animal("fox", "Mammal", "Carnivore", "BC", "LC", "20000", "Native", "Woodlands")
+        animalList = {ZebraMussel, Moose, fox}
+        result_Moose = get_animal_description(animalList, "Zebra Mussel")
+        self.assertEqual(result_Moose, [ZebraMussel])
+
+        result_Fox = get_animal_description(animalList, "fox")
+        self.assertEqual(result_Fox, [fox])
+
+    
 if __name__ == '__main__':
     unittest.main()
